588cd54c1b8c7e6dbcf83a90ccb97d300b93ab1c
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2020-01-16 13:42:16 +0100

    Modified external script

--- scripts/external.sh
+++ scripts/external.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-IN_CONNECTION_ID=30
-IN_QUERY=""
+IN_CONNECTION_ID=$1
+IN_QUERY="$2"
+#LOG_MESSAGES="true"
+LOG_MESSAGES="false"
+
+function logMessage(){
+        local IN_MESSAGE=$1
+
+        if [[ $LOG_MESSAGES = "true" ]]
+        then
+                echo $IN_MESSAGE
+        fi
+}
+
+function logMessageAndExist(){
+        local IN_MESSAGE=$1
+
+        logMessage "$IN_MESSAGE"
+
+        exit 1
+}
 
 function trim(){
         local IN_STRING=$1
@@ -14,7 +33,7 @@ function removeTempFile(){
 
         # Removes the temp file
         if [ -f ${IN_TEMP_FILE} ]; then
-                echo "DEBUG: Removing file: $IN_TEMP_FILE"
+                logMessage "DEBUG: Removing file: $IN_TEMP_FILE"
                 rm ${IN_TEMP_FILE}
         fi
 }
@@ -27,23 +46,25 @@ TEMP_FILE="salida"
 removeTempFile $TEMP_FILE
 
 psql -c "select * from os.ext_connection where id=$IN_CONNECTION_ID" -t >> $TEMP_FILE
-ROWS=$(cat salida | wc -l)
+ROWS=$(cat $TEMP_FILE | wc -l)
+
+# Checks that there is only one line
+if [[ $ROWS = "1" ]]
+then
+        logMessageAndExist "ERROR: No connection defined for the id: $IN_CONNECTION_ID"
+fi
 
 CONNECTION_TYPE=$(cat $TEMP_FILE | cut -d"|" -f2)
 CONNECTION_TYPE=$(trim $CONNECTION_TYPE)
 
-#if [[ ! -v CONNECTION_TYPE ]] || [[ -z "$CONNECTION_TYPE" ]]; then
-#       echo "ERROR: The CONNECTION_TYPE cannot be empty. Exiting..."
-#       exit 1
-#fi
-
+# Checks the conneciton type
+#
 CONNECTION_TYPE=$(toLowerCase $CONNECTION_TYPE)
-if ! [[ $CONNECTION_TYPE = "sqlserver" ]]
+if ! [[ $CONNECTION_TYPE = "azure" ]]
 then
-        echo "ERROR: The Connection Type: $CONNECTION_TYPE is not supported. Only sqlserver is supported. Existing..."
-        exit 1
+        logMessageAndExist "ERROR: The Connection Type: $CONNECTION_TYPE is not supported. Only azure is supported. Existing..."
 else
-        echo "DEBUG: CONNECTION_TYPE: $CONNECTION_TYPE"
+        logMessage "DEBUG: CONNECTION_TYPE: $CONNECTION_TYPE"
 fi
 
 SERVER=$(cat $TEMP_FILE | cut -d"|" -f3)
@@ -57,21 +78,21 @@ DATABASE=$(trim $DATABASE)
 USER=$(cat $TEMP_FILE | cut -d"|" -f7)
 USER=$(trim $USER)
 PASSWORD=$(cat $TEMP_FILE | cut -d"|" -f8)
-PASSWORD=$(trim $PASSWROD)
-echo "DEBUG: Server: $SERVER"
-echo "DEBUG: Instance: $INSTANCE"
-echo "DEBUG: Port: $PORT"
-echo "DEBUG: Database: $DATABASE"
-echo "DEBUG: User: $USER"
-echo "DEBUG: Pass: *****"
+PASSWORD=$(trim $PASSWORD)
+
+logMessage "DEBUG: Server: $SERVER"
+logMessage "DEBUG: Database: $DATABASE"
+logMessage "DEBUG: Query: $IN_QUERY"
+logMessage "DEBUG: User: $USER"
+logMessage "DEBUG: Pass: $PASSWORD"
 removeTempFile $TEMP_FILE
 
-#java   -classpath /usr/local/os/jar/planeta-azure-1.0.0.jar:/usr/local/os/jar/mssql-jdbc-7.0.0.jre8.jar \
-#       -Xms128m \
-#       -Xmx256m \
-#       org.planeta.azure.App \
-#       $SERVER \
-#       $DATABASE \
-#       $USER \
-#       $PASSWORD \
-#       "$QUERY"
+java    -classpath /usr/local/os/jar/planeta-azure-1.0.0.jar:/usr/local/os/jar/mssql-jdbc-7.0.0.jre8.jar \
+        -Xms128m \
+        -Xmx256m \
+        org.planeta.azure.App \
+        $SERVER \
+        $DATABASE \
+        $USER \
+        $PASSWORD \
+        "$IN_QUERY"
---
+++ scripts/install.sql
@@ -0,0 +1,17 @@
+ALTER TABLE os.ext_connection DROP CONSTRAINT ext_check;
+ALTER TABLE os.ext_connection DROP CONSTRAINT ext_check_port;
+
+ALTER TABLE os.ext_connection ADD CONSTRAINT ext_check CHECK ((type = 'oracle'::text AND database_name IS NOT NULL AND instance_name IS NULL) OR (type = 'greenplum'::text AND database_name IS NOT NULL AND instance_name IS NULL) OR (type = 'sqlserver'::text AND database_name IS NULL) OR (type = 'azure'::text AND database_name IS NOT NULL));
+ALTER TABLE os.ext_connection ADD CONSTRAINT ext_check_port CHECK ((port > 0 AND type = 'oracle'::text) OR (port > 0 AND type = 'greenplum'::text) OR (type = 'sqlserver'::text AND port IS NULL) OR (type = 'azure'::text AND port IS NULL));
+  
+INSERT INTO os.ext_connection (type,server_name,database_name, user_name, pass) VALUES ('azure', 'dynamicsplaneta-talend-sql.database.windows.net', 'Dyn_vd_talend_db', 'BitalentUser', 'PASSWORD');
+
+CREATE EXTERNAL WEB TABLE dwtalend.ext_pdw_hotels_borrar1
+(
+  id TEXT
+)
+EXECUTE E'/home/gpadmin/external.sh 18 "SELECT Id FROM dbo.pdw_hotels"' ON MASTER 
+ FORMAT 'text' (delimiter '|' null 'null' escape '\\')
+ENCODING 'UTF8';
+ALTER TABLE dwtalend.ext_pdw_hotels_borrar1
+  OWNER TO gpadmin;
